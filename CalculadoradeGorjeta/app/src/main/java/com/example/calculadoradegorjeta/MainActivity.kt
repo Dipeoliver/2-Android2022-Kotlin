ce53ae63bf49016678daac3182b43c8a7edaec65
Diego Pereira Oliveira <diego.oliveira@venturus.org.br>
2022-08-25 15:55:46 -0300

    fixed app Calculadora de Gorjeta

--- CalculadoradeGorjeta/app/src/main/java/com/example/calculadoradegorjeta/MainActivity.kt
+++ CalculadoradeGorjeta/app/src/main/java/com/example/calculadoradegorjeta/MainActivity.kt
@@ -1,49 +1,70 @@
 package com.example.calculadoradegorjeta
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.AbsSeekBar
+import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 private lateinit var txtValorEntrada: TextInputEditText
 
-private lateinit var seekBarw: AbsSeekBar
-private lateinit var txtGorjeta: TextView
-private lateinit var txtTotal: TextView
-private lateinit var txtseekbar: TextView
-private val Valor = 0.0
-
+private lateinit var txtEntrada: TextView
+private lateinit var txtSeekbar: TextView
+private lateinit var seekBar: SeekBar
+private lateinit var edtGorjeta: EditText
+private lateinit var edtResultado: EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        seekBarw = findViewById(R.id.seekBar)
-        txtGorjeta = findViewById(R.id.etxt_Gorjeta)
-        txtTotal = findViewById(R.id.etxt_ValorTotal)
-        txtseekbar = findViewById(R.id.txt_ValorEntrada)
+        txtEntrada = findViewById(R.id.txt_ValorEntrada)
+        txtSeekbar = findViewById(R.id.txtSeekbar)
+        seekBar = findViewById(R.id.seekBar)
+        edtGorjeta = findViewById(R.id.edt_Gorjeta)
+        edtResultado = findViewById(R.id.edt_Resultado)
 
+        calcListener()
     }
 
     fun calcListener() {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            @SuppressLint()
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progressValue: Int,
+                fromUser: Boolean
+            ) {
+                // here, you react to the value being set in seekBar
+                if (txtEntrada.text.isNullOrEmpty()) {
+                    txtEntrada.text = 0.toString();
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Digitar um valor",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    txtSeekbar.text = "$progressValue%"
 
+                    val valorEntrada = (txtEntrada.text.toString()).toFloat()
+                    val valorGorjeta = (valorEntrada * progressValue) / 100
+                    val valorFinal = (valorEntrada + valorGorjeta)
+                    edtGorjeta.setText(valorGorjeta.toString())
+                    edtResultado.setText(valorFinal.toString())
+                }
+            }
 
-//        seekBarw.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-//            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-//                // here, you react to the value being set in seekBar
-//            }
-//
-//            override fun onStartTrackingTouch(seekBar: SeekBar) {
-//                // you can probably leave this empty
-//            }
-//
-//            override fun onStopTrackingTouch(seekBar: SeekBar) {
-//                // you can probably leave this empty
-//            }
-//        })
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // you can probably leave this empty
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // you can probably leave this empty
+            }
+        })
     }
-
 }
